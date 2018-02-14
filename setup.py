04a928ee6a45e1d7cb78f6c9a6286d25e87bbbb7
Andreas Jung <info@zopyx.com>
2018-02-14 11:02:20 +0100

    updated

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     install_requires=[
         # -*- Extra requirements: -*-
         'plone.api',
+        'plone.app.dexterity',
         'Products.GenericSetup>=1.8.2',
         'setuptools',
         'z3c.jbot',
