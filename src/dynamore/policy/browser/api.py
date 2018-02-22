73c59fd4c12d893e0e1ea68b766e10205628528b
Andreas Jung <info@zopyx.com>
2018-02-22 08:01:16 +0100

    provisioning API

--- src/dynamore/policy/browser/api.py
+++ src/dynamore/policy/browser/api.py
@@ -1,8 +1,31 @@
+
+import json
 from Products.Five.browser import BrowserView
+from Products.CMFPlone.factory import addPloneSite
 import zExceptions
 
 
 class API(BrowserView):
+
+    def recreate_plone_site(self):
+        """ Recreate a Plone site """
+
+        data = json.loads(self.request.BODY)
+        site_id = str(data['site_id'])
+        extension_ids = data['extension_ids']
+
+        root = self.context.restrictedTraverse('/')
+        if site_id in root.objectIds():
+            print 'Deleting Plone site "{0}"'.format(site_id)
+            root.manage_delObjects([site_id])
+
+        print 'Creating Plone site "{0}" with {1}'.format(site_id, extension_ids)
+        addPloneSite(root, site_id, extension_ids=extension_ids)
+        print 'Created Plone site "{0}" with {1}'.format(site_id, extension_ids)
+
+        self.request.response.setStatus(201)
+        self.request.response.write('Created')
+
 
     def remote_exists(self, path):
         """ Check if `path` exists based on our own traversal.
